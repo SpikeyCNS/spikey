ca92ab337ff71a4ccb91f8aea0c0f3afaa05ae49
csdhv9 <csdhv9@umsystem.edu>
2021-01-28 15:02:11 -0600

    Update input docs.

--- spikey/snn/input/track_rbf.py
+++ spikey/snn/input/track_rbf.py
@@ -12,17 +12,56 @@ class TrackRBF(Input):
 
     Parameters
     ----------
-    n_inputs: int
-        Number of inputs.
+    kwargs: dict
+        Dictionary with values for each key in NECESSARY_KEYS.
+
+    Usage
+    -----
+    ```python
+    processing_time = 10
+    config = {
+        "n_inputs": 10,
+        "magnitude": 2,
+        "firing_steps": -1,
+        "input_pct_inhibitory": 0.2,
+    }
+    input = TrackRBF(**config)
+    env = LinearTrack()
+
+    state = env.reset()
+    for step in range(10):
+        input.update(state)
+
+        for _ in range(processing_time)
+            in_fires = input.__call__()
+
+        state, _, done, __ = env.update(0)
+
+        if done:
+            break
+    ```
+
+    ```python
+    class network_template(Network):
+        config = {
+            "n_inputs": 10,
+            "magnitude": 2,
+            "firing_steps": -1,
+            "input_pct_inhibitory": 0.2,
+        }
+        _template_parts = {
+            "input": TrackRBF
+        }
+    ```
     """
 
     def __call__(self) -> np.ndarray:
         """
-        Spike output for each input neuron.
+        Spikes output from each input neuron.
 
         Returns
         -------
-        ndarray Spike output for each neuron.
+        ndarray[n_inputs] Spike output for each neuron.
         """  ## Does not respect polarities.
         if not self.values.size:
             return []
@@ -36,12 +75,12 @@ class TrackRBF(Input):
 
     def update(self, state: object):
         """
-        Update input settings.
+        Update input generator.
 
         Parameters
         ----------
-        state: list of float
-            Discretized enviornment state.
+        state: object
+            Enviornment state in format generator can understand.
         """
         x, xdot, y, ydot = state
 
@@ -56,9 +95,5 @@ class TrackRBF(Input):
         self.values = np.zeros((43, 5))
         for (m, n), _ in np.ndenumerate(self.values):
             self.values[m, n] = p_t(m, n)
-
-        # import cv2
-        # cv2.imshow("", self.values)
-        # cv2.waitKey(1)
 
         self.values = np.ravel(self.values)
