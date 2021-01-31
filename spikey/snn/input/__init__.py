694060f754bfc7f535a32ac2518cb0cb391d90ff
csdhv9 <csdhv9@umsystem.edu>
2021-01-30 21:34:14 -0600

    Remove TrackRBF.

--- spikey/snn/input/__init__.py
+++ spikey/snn/input/__init__.py
@@ -5,6 +5,5 @@ try:
     from spikey.snn.input.ratemap import RateMap
     from spikey.snn.input.staticmap import StaticMap
     from spikey.snn.input.rbf import RBF
-    from spikey.snn.input.track_rbf import TrackRBF
 except ImportError as e:
     raise ImportError(f"input/__init__.py failed: {e}")
