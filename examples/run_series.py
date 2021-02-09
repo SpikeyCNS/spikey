bb02e3a5fd3b0af5001163da0f8605ca64e6b63d
csdhv9 <csdhv9@umsystem.edu>
2021-02-08 18:40:47 -0600

    Update series docs.

--- examples/run_series.py
+++ examples/run_series.py
@@ -1,7 +1,5 @@
 """
 Run a set of experiments -- optionally in parallel.
-
-NOTE: Windows may require to run in administrator.
 """
 import os
 import spikey
@@ -15,15 +13,10 @@ from spikey.experiments.florian_rate import (
 if __name__ == "__main__":
     experiment_list = {
         "control": None,
-        #'trace_decay': ('trace_decay', [.01, .02, .03, .04, .05]),
-        #'potential_decay': ('potential_decay', [.05, .1, .2, .4, .6, .8]),
-        #'prob_rand_fire': ('prob_rand_fire', [0, .05, .1, .25]),
-        #'refractory_period': ('refractory_period', [0, 1, 3, 6]),
         #'stdp_window': ('stdp_window', [20, 30, 40, 50]),
         #'learning_rate': ('learning_rate', [.025, .075, .15, .25]),
-        #'input': [('input', [UniformSpikeGenerator, PoissonSpikeGenerator]), ('rate_mapping', [[0, .4], [0, .165]])],
     }
-    N_REPEATS = 4
+    N_REPEATS = 2
 
     MAX_PROCESS = 2
 
