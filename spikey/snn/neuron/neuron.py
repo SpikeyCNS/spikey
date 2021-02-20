71e2a187f2300880d46798d110b7f2453ab10719
csdhv9 <csdhv9@umsystem.edu>
2021-02-20 03:26:37 -0600

    Update neuron docs.

--- spikey/snn/neuron/neuron.py
+++ spikey/snn/neuron/neuron.py
@@ -1,4 +1,10 @@
 """
-Meant to bypass issues w/ importing template.
+A group of spiking neurons.
+
+Each spiking neuron has an internal membrane potential that
+increases with each incoming spike. The potential persists but slowly
+decreases over time. Each neuron fires when its potential surpasses
+some firing threshold and does not fire again for the duration
+of its refractory period.
 """
 from spikey.snn.neuron.template import Neuron
