f691898d4112128dae30a7b3f3ccba814b8eeb62
csdhv9 <csdhv9@umsystem.edu>
2021-02-23 00:34:39 -0600

    Cleanup spike_raster, reduce parameters.

--- spikey/viz/raster.py
+++ spikey/viz/raster.py
@@ -6,85 +6,68 @@ import matplotlib.pyplot as plt
 
 
 def spike_raster(
-    spikes: np.ndarray,
-    step_len: int,
-    steps: int = None,
-    rewards: np.ndarray = None,
-    input_polarities: np.ndarray = None,
-    polarities: np.ndarray = None,
-    n_inputs: int = None,
+    spike_log: np.bool = None,
+    rewards: np.float = None,
+    polarities: np.int = None,
+    callback: object = None,
 ):
     """
     Render a spike raster plot.
+    Fires are white, inhibitories tinted red.
 
     Parameters
     ----------
-    spikes: ndarray[steps, neurons]
+    spike_log: ndarray[steps, neurons]
         Spike train.
-    step_len: int
-        Number of network updates per step.
-    steps: int, default=len(spikes)
-        Number of game steps to plot.
     rewards: ndarray[steps]
         Rewards at each step.
     polarities: ndarray[neurons]
-        Polaritiy of neurons.
-    n_inputs: int
-        Number of input neurons, serves as offset for polarities.
+        Polaritiy of(optionally inputs and) neurons.
+    network: Network, default=None
+        Network to give spike_log and polarities if not explicitly given.
+    callback: ExperimentCallback, default=None
+        Callback to give rewards if not explicitly given.
 
     Usage
     -----
     ```python
-    spike_raster(network.spike_log, network._processing_time)
+    spike_raster(network, game)
     ```
     """
-    steps = steps or len(spikes)
+    try:
+        spike_log = spike_log or network.spike_log        
+    except:
+        raise UnboundLocalError("Need to define either spike_log or network for raster!")
+    polarities = polarities or network._polarities
+    rewards = rewards or callback.info['step_rewards']
 
-    output = None
-
-    ## Spikes
-    # fire is white, inhibitories tinted red
-    spikes = np.abs(spikes[-(steps * step_len) :]).T
-    spikes = np.dstack((spikes, spikes, spikes))
-
-    output = spikes
-
-    if input_polarities is not None:
-        inh_locs = np.where(input_polarities == -1)[0]
-        spikes[:n_inputs][inh_locs, :, 0] = 0.5
-        spikes = np.abs(spikes)  # Fix weird bug setting b,g = -1
+    spike_log = np.abs(spike_log).T
+    spike_log = np.dstack((spike_log, spike_log, spike_log))
 
     if polarities is not None:
-        if n_inputs is None:
-            print("Warning: May need n_inputs for polarities to be placed correctly!")
-
         inh_locs = np.where(polarities == -1)[0]
-        spikes[n_inputs:][inh_locs, :, 0] = 0.5
-        spikes = np.abs(spikes)  # Fix weird bug setting b,g = -1
+        if len(polarities == len(spike_log)):
+            spike_log[inh_locs, :, 0] = 0.5
+        else:
+            spike_log[n_inputs:][inh_locs, :, 0] = 0.5
+        spike_log = np.abs(spike_log)  # Fix weird bug setting b,g = -1
 
     if rewards is not None:
-        ## Rewards
-        # - is red, 0 is black, + is green
-        rewards = np.array(rewards)
-
-        reward_helper = rewards[-steps:].reshape((-1, 1))
-
+        reward_helper = rewards.reshape((-1, 1))
         reward_helper = np.ravel(np.hstack([reward_helper for _ in range(step_len)]))
-
         rewards = np.zeros(shape=(reward_helper.size, 3))
 
         rewards[np.where(reward_helper < 0)] = np.array([1, 0, 0])
         rewards[np.where(reward_helper == 0)] = np.array([0, 0, 0])
         rewards[np.where(reward_helper > 0)] = np.array([0, 1, 0])
 
-        if len(rewards.shape) < len(spikes.shape):
+        if len(rewards.shape) < len(spike_log.shape):
             rewards = rewards.reshape((1, *rewards.shape))
 
-        output = np.vstack((spikes, rewards))
+        output = np.vstack((spike_log, rewards))
 
-    #
     fig = plt.figure()
-    plt.gcf().canvas.set_window_title(f"Spike Raster Plot, past {steps} steps")
+    plt.gcf().canvas.set_window_title(f"Spike Raster")
 
     ax = fig.add_subplot(111)
     ax.set_title(f"Raster{' w/ Rewards' if rewards is not None else ''}")
