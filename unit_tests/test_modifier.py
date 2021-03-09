b68680f57de5c9240d2e863fe5db923c552bb41a
csdhv9 <csdhv9@umsystem.edu>
2021-03-09 15:43:19 -0600

    Add base empty modifier test.

---
+++ unit_tests/test_modifier.py
@@ -0,0 +1,20 @@
+"""
+Tests for modifier.
+"""
+import unittest
+from unit_tests import ModuleTest
+import numpy as np
+from spikey.snn import modifier
+
+
+class TestModifier(unittest.TestCase, ModuleTest):
+    """
+    Tests for modifier.
+    """
+
+    TYPES = [modifier.template.Modifier]
+    BASE_CONFIG = {"param": "processing_time"}
+
+
+if __name__ == "__main__":
+    unittest.main()
