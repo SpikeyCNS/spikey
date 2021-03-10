40042ca1538402e4c1ab4beee77c14d0029aad51
csdhv9 <csdhv9@umsystem.edu>
2021-03-09 20:25:59 -0600

    Add test readout.

---
+++ unit_tests/test_readout.py
@@ -0,0 +1,37 @@
+"""
+Tests for snn.Readout.
+"""
+import unittest
+from unit_tests import ModuleTest
+import numpy as np
+from spikey.snn import readout
+
+
+class TestReadout(unittest.TestCase, ModuleTest):
+    """
+    Tests for snn.Readout.
+    """
+
+    TYPES = [readout.Threshold, readout.NeuronRates, readout.PopulationVector]
+    BASE_CONFIG = {
+        "n_outputs": 2,
+        "magnitude": 1,
+        "action_threshold": 0.5,
+        "output_range": [0, 1],
+        "n_actions": 1,
+    }
+
+    @ModuleTest.run_all_types
+    def test_usage(self):
+        readout = self.get_obj()
+        readout.reset()
+
+        log_shape = (10, self.BASE_CONFIG['n_outputs'])
+        action = readout(np.ones(log_shape))
+        action = readout(np.random.uniform(0, 1, size=log_shape))
+        action = readout(np.random.uniform(-5, 5, size=log_shape))
+
+
+
+if __name__ == "__main__":
+    unittest.main()
