0e93766278b7beb244a72d42fa4fe083ff436dbe
csdhv9 <csdhv9@umsystem.edu>
2021-03-26 21:00:04 -0500

    Update setup.

--- setup.py
+++ setup.py
@@ -10,18 +10,26 @@ assert (
 
 setup_args = {
     "name": "spikey",
-    "version": "0.5.00",  # x.x.xxxx
-    "description": "Spikey is a malleable ndarray based spiking neural network framework and training platform.",
+    "version": "0.5.00",
+    "description": "Spikey is a malleable spiking neural network framework and training platform.",
     "license": "MIT",
     "author": "Cole",
     "author_email": "csdhv9@umsystem.edu",
     "packages": ["spikey"],
     "install_requires": ["numpy", "pandas", "matplotlib"],
+	"python_requires": ">=3.6",
+	"url": "https://github.com/SpikeyCNS/spikey",
+	"classifiers": [
+		"Programming Language :: Python :: 3.8",
+		"License :: MIT License",
+		"Operating System :: OS Independent",
+	],
 }
 
 
 if __name__ == "__main__":
     with open("README.md", "r") as f:
         setup_args["long_description"] = f.read()
+        setup_args["long_description_content_type"] = "text/markdown"
 
     setup(**setup_args)
