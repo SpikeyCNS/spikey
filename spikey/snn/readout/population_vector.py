57abd3b643185c0d6b1c0d0020588939ce6ac296
csdhv9 <csdhv9@umsystem.edu>
2021-05-01 23:00:28 -0500

    Remove decay kernel from population vector.

--- spikey/snn/readout/population_vector.py
+++ spikey/snn/readout/population_vector.py
@@ -70,19 +70,12 @@ class PopulationVector(Readout):
             return np.zeros(self._n_actions)
 
         spikes = np.where(output_spike_train, 1, 0)
-
-        T_gamma, V_gamma = 50, 20
-        gamma = lambda t: (np.e ** (-t / T_gamma) - np.e ** (-t / V_gamma)) / (
-            T_gamma - V_gamma
-        )
-        decay_kernel = np.vectorize(gamma)(np.arange(spikes.shape[0])).reshape((-1, 1))
-
-        firing_rates = np.sum(spikes * decay_kernel, axis=0)
+        spike_counts = np.sum(spikes, axis=0)
 
         group_size = self._n_outputs // self._n_actions
 
         p = [
-            np.sum(firing_rates[i * group_size : (i + 1) * group_size])
+            np.sum(spike_counts[i * group_size : (i + 1) * group_size])
             for i in range(self._n_actions)
         ]
         p = np.array(p)
