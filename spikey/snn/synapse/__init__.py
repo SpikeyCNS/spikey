c132c798d9a5f163ee050d4c0d4cf7bc4c9f4847
csdhv9 <csdhv9@umsystem.edu>
2021-06-26 16:38:06 -0500

    Remove florian.

--- spikey/snn/synapse/__init__.py
+++ spikey/snn/synapse/__init__.py
@@ -3,6 +3,5 @@ Synapse __init__.
 """
 try:
     from spikey.snn.synapse.rlstdp import RLSTDP, LTP
-    from spikey.snn.synapse.florian import FlorianSTDP
 except ImportError as e:
     raise ImportError(f"synapse/__init__.py failed: {e}")
