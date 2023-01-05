a85ecd752e669a213b8c4d1e1347dd6e0cd51455
Cole Dieckhaus <csdhv9@umsystem.edu>
2023-01-04 21:50:13 -0800

    [6/5] Ray cleanup // Get tests passing (#34)

--- unit_tests/test_game.py
+++ unit_tests/test_game.py
@@ -7,7 +7,6 @@ from copy import deepcopy
 from gym.envs.classic_control import cartpole, mountain_car
 from spikey import Key
 from spikey import games
-from spikey.meta import metagames
 
 
 class FakeTrainingLoop:
@@ -76,62 +75,6 @@ class TestRL(unittest.TestCase, ModuleTest):
             state, reward, done, info = game.step(0)
             if done:
                 break
-
-        game.close()
-
-
-class TestMetaRL(unittest.TestCase, ModuleTest):
-    """
-    Tests for games.MetaRL.
-    """
-
-    TYPES = [
-        metagames.MetaNQueens,
-        metagames.EvolveNetwork,
-    ]
-    BASE_CONFIG = {
-        "training_loop": FakeTrainingLoop(),
-        "genotype_constraints": {"a": [1, 2, 3]},
-        "win_fitness": 1,
-        "fitness_getter": fitness_getter,
-    }
-
-    @ModuleTest.run_all_types
-    def test_init(self):
-        game_type = type(self.get_obj())
-
-        class game_template(game_type):
-            NECESSARY_KEYS = game_type.extend_keys(
-                [
-                    Key("a", "a"),
-                    Key("b", "b"),
-                ]
-            )
-            config = deepcopy(self.BASE_CONFIG)
-            config.update({"a": 10, "b": 20})
-
-        a = 11
-        game = game_template(a=a)
-        self.assertEqual(game.params["a"], a)
-        self.assertEqual(game.params["b"], game_template.config["b"])
-
-    @ModuleTest.run_all_types
-    def test_usage(self):
-        game = self.get_obj()
-
-        state = game.reset()
-        for _ in range(100):
-            genotype = {key: 0 for key in game.GENOTYPE_CONSTRAINTS.keys()}
-            try:
-                fitness = game.get_fitness(genotype)
-            except AssertionError as e:
-                pass
-            try:
-                state, fitness, done, info = game.step(genotype)
-                if done:
-                    break
-            except AssertionError as e:
-                pass
 
         game.close()
 
